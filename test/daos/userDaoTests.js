fd3d071257b5f41d72535b3bb3d2efb2984b4597
matias feld <matias.feld@mdevz.com>
2019-03-17 20:59:32 -0300

    added more tests to User Dao

--- test/daos/userDaoTests.js
+++ test/daos/userDaoTests.js
@@ -1,11 +1,15 @@
-var expect = require('chai').expect;
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised');
+const expect = chai.expect;
+chai.use(chaiAsPromised);
+
 var UserDao = require('../../src/daos/UserDao');
 
 describe('"UserDao Tests"', () => {
 
-    describe('Method: Register User without picture', () => {
+    describe('Register User without picture', () => {
 
-        var data = {name: "Pepe", surname: "Perez", email:"pepe@gmail.com"}; 
+        var data = {name: "Pepe", surname: "Perez", email:"pepe@gmail.com"};
         var user;
 
         beforeEach(async () => {
@@ -20,12 +24,35 @@ describe('"UserDao Tests"', () => {
             expect(user.name).to.eq("Pepe");
         });
 
-        it('user surname and must be Perez', async () => {
+        it('user surname must be Perez', async () => {
             expect(user.surname).to.eq("Perez");
         });
 
-        it('user email and must be pepe@gmail.com', async () => {
+        it('user email must be pepe@gmail.com', async () => {
             expect(user.email).to.eq("pepe@gmail.com");
+        });
+    });
+
+    describe('Register User with errors', () => {
+
+        it('empty user must not be registered', async () => {
+            var data = {};
+            expect(UserDao.create(data)).to.eventually.be.rejected;
+        });
+
+        it('user must not be registered without name', async () => {
+            var data = {surname: "Perez", email:"pepe@gmail.com"};
+            expect(UserDao.create(data)).to.eventually.be.rejected;
+        });
+
+        it('user must not be registered without surname', async () => {
+            var data = {name: "Pepe", email:"pepe@gmail.com"};
+            expect(UserDao.create(data)).to.eventually.be.rejected;
+        });
+
+        it('user must not be registered without email', async () => {
+            var data = {name: "Pepe", surname: "Perez"};
+            expect(UserDao.create(data)).to.eventually.be.rejected;
         });
     });
 
