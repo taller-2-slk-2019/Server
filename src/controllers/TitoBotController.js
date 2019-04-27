a35236c7d3ee31cd8c76d28ae6640c95e6d6356b
matias feld <matias.feld@mdevz.com>
2019-04-27 16:27:04 -0300

    tito bot send welcome message

--- src/controllers/TitoBotController.js
+++ src/controllers/TitoBotController.js
@@ -1,3 +1,5 @@
+var logger = require('logops');
+const axios = require('axios');
 var BotsController = require('./BotsController');
 
 class TitoBotController {
@@ -12,6 +14,22 @@ class TitoBotController {
         };
 
         BotsController.sendMessageToBot(bot, message);
+    }
+
+    async userAddedToChannel(channel, user){
+        var data = {
+            bot: this.titoBotName,
+            channelId: channel.id,
+            userId: user.id
+        };
+
+        var url = this.titoBotBaseUrl + 'welcome';
+
+        logger.info(`Tito bot: sending welcome to user ${user.id} in channel ${channel.id}`);
+        axios.post(url, data).catch(err => {
+            logger.error('Failed message to tito bot');
+            logger.error(err);
+        });
     }
 }
 
