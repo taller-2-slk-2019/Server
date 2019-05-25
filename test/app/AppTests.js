40d50dfc36d6ceb0554cc6253b1cf18c20d48f2d
matias feld <matias.feld@mdevz.com>
2019-05-25 20:37:18 -0300

    added tests

---
+++ test/app/AppTests.js
@@ -0,0 +1,22 @@
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised');
+const expect = chai.expect;
+chai.use(chaiAsPromised);
+const { stub, assert } = require('sinon');
+const sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+const request = require('supertest');
+const app = require('../../src/app')
+
+describe('"App Tests"', () => {
+
+    describe('Invalid request', () => {
+        it('should return 404', async () => {
+            var response = await request(app)
+                                .get('/invalid')
+            
+            expect(response.status).to.eq(404);
+            expect(response.text).to.eq('Invalid Api');
+        });
+    });
+});
